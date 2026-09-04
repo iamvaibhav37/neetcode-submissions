3b684cd54e7e810b5c8d696895633e0f54050d66
iamvaibhav37 <be22b041@smail.iitm.ac.in>
2026-09-04 23:42:47 +0530

    Add: populating-next-right-pointers-in-each-node - submission-0

---
+++ Data Structures & Algorithms/populating-next-right-pointers-in-each-node/submission-0.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        res = [] 
+        q = deque([root])
+
+        while q:
+            level = []
+            for i in range(len(q)):
+                node = q.popleft()
+                if node:
+                    level.append(node)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                for i in range(len(level)-1):
+                    level[i].next = level[i+1]
+                level
+        return root
+
+
